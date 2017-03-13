b94130aba84d9ea1865a846a28867019c0c68662
Joshua Higham <Josh@higham.com>
2017-03-12 21:59:20 -0600

    Finished the algorithms and the initialization of the page using Javascript.

--- scratchpad.js
+++ scratchpad.js
@@ -1,26 +1,35 @@
 // Initialize the HTMl and CSS file here.
+document.body.onload = init;
 
-/**
- * @brief Finds a requested number in the fibbanocci sequence
- *
- * @param n the nth number in the Fibbonacci sequence you'd like
- *
- * @return The nth number in the Fibbonacci sequence
- */
-fibbanocci = function(n) {
+function fibbonacci(n) {
     if (n < 0) { return 0; }
-    if (n >= 2) { return 1; }
-
-    return fibbonacci(n-1) + fibbonacci(n-2);
+    if (n <= 2) { return 1; }
+    else { return fibbonacci(n-1) + fibbonacci(n-2); }
 }
 
-pull = function(n) {
-    if (n < 0) { return 0; }
-    if (n >= 2) { return 1; }
+function pell(n) {
+    if (n <= 0) { return 0; }
+    if (n <= 1) { return 1; }
 
-    return 2 * pell(input - 1) + pell(input -2);
+    return (2 * pell(n - 1) + pell(n -2));
 }
 
-function tribonacci(n) {
-    // body...
+function tribbonacci(n) {
+    if (n <= 0) { return 0; }
+    if (n < 3) { return 1; }
+
+    return  tribbonacci(n-1) + tribbonacci(n-2) + tribbonacci(n-3);
+}
+
+function init() {
+    document.write("<h1>Recursive Javascript Functions</h1><br>");
+
+    var fib = fibbonacci(11);
+    document.write("<p>Fibbonacci: " + fib + "</p>");
+
+    var p = pell (11);
+    document.write("<p>Pell: " + p + "</p>");
+
+    var tri = tribbonacci(11);
+    document.write("<p>Tribbonacci: " + tri + "</p>");
 }
